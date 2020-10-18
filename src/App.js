b851d150e916696087d746460b3c56365f318cc2
Mussawer <mmussawerahmed@gmail.com>
2020-10-19 03:08:20 +0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 // import React, { Component } from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div class name="container">
-          <NavbarBrand href="/">I changed the text now</NavbarBrand>
+          <NavbarBrand href="/">Restaurant</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
