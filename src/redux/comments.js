019f9b7f44346b763f49f48218b050c716bfe746
Mussawer <mmussawerahmed@gmail.com>
2020-11-17 16:24:53 +0500

    Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,9 @@
+import { actionTypes } from 'react-redux-form';
+import { COMMENTS } from '../shared/comments';
+
+export const Comments = (state = COMMENTS, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
